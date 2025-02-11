d7bf5d455523b2fa0f235423edbefe57cdb59a04
Yashew <yash.2005.cr@gmail.com>
2025-02-11 22:05:17 +0530

    mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,32 @@
-import e from "express";
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-const app = e();
-const PORT = 8080;
+dotenv.config()
 
-app.get("/", (req,res) => {
-    res.send("hehe")
-})
+const app = express();
+const PORT = process.env.PORT || 8080
+const MONGO_URL = process.env.MONGO_URL 
+
+mongoose
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+app.use(
+  cors({
+    origin: ["http://localhost:4000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+  
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT}`);
 });
+
